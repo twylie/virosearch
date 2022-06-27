84c8e5e53857c93fd285cb5537272b1af61e4662
tnwylie <twylie@wustl.edu>
2022-06-27 14:34:17 -0500

    Removed older cmd files for references.

--- ref_EBV/referencesDB/NC_007605.1/NC_007605.1.fna.cmd
+++
@@ -1,1 +0,0 @@
-ln -s /pwd/ref_EBV/referencesDB/NC_007605.1/NC_007605.1.fna* /tmp/results/smk/02_link_reference_genomes
--- ref_EBV/referencesDB/NC_009334.1/NC_009334.1.fna.cmd
+++
@@ -1,1 +0,0 @@
-ln -s /pwd/ref_EBV/referencesDB/NC_009334.1/NC_009334.1.fna* /tmp/results/smk/02_link_reference_genomes
